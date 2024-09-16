91ede76854117503f50f274e7f0c165706e020d6
hemang jagani? <hemangjagani@gmail.com>
2024-09-16 11:49:59 -0400

    Completed: Algolia search

--- medusa-config.js
+++ medusa-config.js
@@ -52,6 +52,38 @@ const plugins = [
       },
     },
   },
+  // Plugin for algolia
+  {
+    resolve: `medusa-plugin-algolia`,
+    options: {
+      applicationId: process.env.ALGOLIA_APP_ID,
+      adminApiKey: process.env.ALGOLIA_ADMIN_API_KEY,
+      settings: {
+        products: {
+          indexSettings: {
+            searchableAttributes: ["title", "description"],
+            attributesToRetrieve: [
+              "id",
+              "title",
+              "description",
+              "handle",
+              "thumbnail",
+              "variants",
+              "variant_sku",
+              "options",
+              "collection_title",
+              "collection_handle",
+              "images",
+            ],
+          },
+          transform: (product) => ({ 
+            id: product.id, 
+            // other attributes...
+          }),
+        },
+      },
+    },
+  },
 ];
 
 const modules = {
